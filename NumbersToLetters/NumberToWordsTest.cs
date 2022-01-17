cbf62271207ea2e25cc624ffa3e03870ca4865c0
Amon Kiptoo Sabul <chepseron@gmail.com>
2022-01-17 11:08:53 +0300

    Add files via upload

---
+++ NumbersToLetters/NumberToWordsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace NumbersToLetters
+{
+    public class NumberToWordsTest
+    {
+        [Fact]
+        public void TestNumbersToWords()
+        {
+            Assert.Throws<Exception>(() => new NumbersToLetters("1000009"));
+
+        }
+
+    }
+}
